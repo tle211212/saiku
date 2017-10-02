06ce0629024ecea21d5ffb359d7f74b1988f281f
dinhnn <dinh.nguyen@fahasa.com>
2017-10-02 09:37:22 +0700

    Remove comment

--- saiku-ui/js/saiku/Settings.js
+++ saiku-ui/js/saiku/Settings.js
@@ -316,5 +316,4 @@ var isMobile = (function(userAgent) {
 /**
  * Extend settings with charts colors
  */
- //Settings.CHART_COLORS = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"];
  Settings.CHART_COLORS = ["#40a3e7", "#8fbf59", "#f6aa4a", "#f4394a", "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"];
